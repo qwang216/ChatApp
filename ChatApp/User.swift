b6c925d6845dc956225a4614eab65bad43e2a44b
Jason Wang <qwang216@gmail.com>
2016-08-14 18:22:09 -0400

    added user model

---
+++ ChatApp/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  ChatApp
+//
+//  Created by Jason Wang on 8/14/16.
+//  Copyright © 2016 Jason Wang. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+}
